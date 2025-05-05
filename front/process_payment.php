3f2faae72e905ad0ffa02fa7919369170620fa39
laamri <122491199+laamri@users.noreply.github.com>
2025-05-05 11:49:28 +0100

    new commit
    change some comment

--- front/process_payment.php
+++ front/process_payment.php
@@ -21,7 +21,7 @@ try {
         throw new Exception('Données de réservation invalides.');
     }
 
-    // Verify product exists and get price
+    // Verify product exists and get price...
     $sql_prod = $pdo->prepare('SELECT prix, discount FROM produit WHERE id = ? AND disponibilite = 1');
     $sql_prod->execute([$idProduit]);
     $produit = $sql_prod->fetch(PDO::FETCH_ASSOC);
@@ -30,7 +30,7 @@ try {
         throw new Exception('Produit non disponible ou introuvable.');
     }
 
-    // Calculate expected price
+    // Calculate expected price after discount
     $prixFinal = $produit['prix'] - ($produit['prix'] * $produit['discount'] / 100);
     $expectedTotal = $prixFinal * $nbReservation;
 
@@ -42,7 +42,7 @@ try {
     $update = $pdo->prepare('UPDATE produit SET disponibilite = 0 WHERE id = ?');
     $update->execute([$idProduit]);
 
-    // Insert reservation into demande
+    // Insert reservation into demandee
     $insertDemande = $pdo->prepare('INSERT INTO demande (id_client, id_produit, prix, nb_reservation) VALUES (?, ?, ?, ?)');
     $insertDemande->execute([$_SESSION['id_client'], $idProduit, $prixTotal, $nbReservation]);
 
